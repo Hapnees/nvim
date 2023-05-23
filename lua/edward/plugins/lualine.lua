d4071598cbec667dda49fcd57dafe7e726b44c71
Hapnees <8dwardm8lton@gmail.com>
2023-05-23 20:51:19 +0300

    Feat (lualine): remove fileformat, encoding; replace filename

--- lua/edward/plugins/lualine.lua
+++ lua/edward/plugins/lualine.lua
@@ -32,9 +32,9 @@ lualine.setup({
 	},
 	sections = {
 		lualine_a = { "mode" },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_b = { "filename" },
+		lualine_c = { "branch", "diff", "diagnostics" },
+		lualine_x = { "filetype" },
 		lualine_y = { "" },
 		lualine_z = { "location" },
 	},
