6dd2a9992e8ba5b3f452f253766f5141e9bb9c36
Hapnees <8dwardm8lton@gmail.com>
2023-06-06 17:41:42 +0300

    Feat (colorscheme): transparent SignColumn; move def icons to colorscheme file

--- lua/edward/plugins/neotree.lua
+++ lua/edward/plugins/neotree.lua
@@ -1,22 +1,19 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
- vim.fn.sign_define("DiagnosticSignError",
-    {text = " ", texthl = "DiagnosticSignError"})
-  vim.fn.sign_define("DiagnosticSignWarn",
-    {text = " ", texthl = "DiagnosticSignWarn"})
-  vim.fn.sign_define("DiagnosticSignInfo",
-    {text = " ", texthl = "DiagnosticSignInfo"})
-  vim.fn.sign_define("DiagnosticSignHint",
-    {text = "", texthl = "DiagnosticSignHint"})
-
 local status, neotree = pcall(require, "neo-tree")
 if not status then
-  return
+	return
 end
 
 neotree.setup({
-  popup_border_style = "rounded",
-  enable_git_status = true,
-  enable_diagnostics = true,
-})
+	popup_border_style = "rounded",
+	enable_git_status = true,
+	enable_diagnostics = true,
 
+	source_selector = {
+		sources = {
+			{ source = "filesystem", display_name = " 󰉓 Files " },
+			{ source = "git_status", display_name = " 󰊢 Git " },
+		},
+	},
+})
